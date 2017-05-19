50a0fa19fbbe2a8cd9b793f712044f4b88875d0b
Bernardo Ferreira <bernardomrferreira@ua.pt>
2017-05-19 17:13:29 +0100

    Config file now has the devices detected be each gateway(hardcoded)

--- configs.py
+++ configs.py
@@ -1,11 +1,57 @@
 HOST = "127.0.0.1"
 HOST_PUB = "sonata4.local"
-CLIENT_ID = "gateway4"
-CLIENT_ID_PUB = "gateway4_pub"
+CLIENT_ID = "gateway2"
+CLIENT_ID_PUB = "gateway2_pub"
 RULES_FOLDER = "resources/json_rules/"
 SUB_TOPIC = '#'
 HB_TOPIC = '/heart_beat'
 RULES_TOPIC = '/SM/rule'
 HB_TIMER = 5
 MAX_MEM = 2000000
-GATEWAY_NAME = 'sonata10.local'
+GATEWAY_NAME = 'gateway-pi2.local'
+DEVICE_AC = []
+DEVICE_TMP = []
+DEVICE_HUM = []
+DEVICE_LUX = []
+DEVICE_MOTION = []
+DEVICE_LIGHT = []
+DEVICE_LIGHT_B = []
+DEVICE_LIGHT_C = []
+DEVICE_LIGHT_S = []
+
+"""
+for i in range(1,46):
+    DEVICES.append('ac%d'%i)
+
+for i in range(1,50):
+    DEVICES.append('tmp%d'%i)
+
+
+for i in range(1,50):
+    DEVICES.append('hum%d'%i)
+
+
+for i in range(1,157):
+    DEVICES.append('lux%d'%i)
+
+"""
+for i in range(110,165):
+    DEVICE_MOTION.append('motion%d'%i)
+
+"""
+for i in range(1,109):
+    DEVICES.append('light%d'%i)
+
+
+for i in range(1,34):
+    DEVICES.append('light_b%d'%i)
+
+
+for i in range(1,10):
+    DEVICES.append('light_s%d'%i)
+"""
+
+for i in range(1,77):
+    DEVICE_LIGHT_C.append('light_c%d'%i)
+
+
